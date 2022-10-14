659a9c2230ee13676cc6a965f251e0b0a8be25ed
lovisa.carling <lovisa.carling@axakon.se>
2022-10-14 14:03:03 +0200

    installed fontawesome and removed react logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
+import {faPlane} from "@fortawesome/free-solid-svg-icons"
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      
+        <FontAwesomeIcon icon={faPlane} size="4x" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Our awesome flight app!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
