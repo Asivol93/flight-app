bc09b780a1ad449dafd185cc6f264e3e966668d7
ThomasAxakon <50200230+ThomasAxakon@users.noreply.github.com>
2022-11-22 22:34:02 +0100

    Added css + validation for inputfield
    Added the first building blocks of new css code for a more clean visual experience. Added helping text & validation requirements for the type of flight numbers

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import './App.css';
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
-import {faPlane} from "@fortawesome/free-solid-svg-icons"
+import {faPlane, faArrowRight} from "@fortawesome/free-solid-svg-icons"
 import { getFlight } from './API/API';
 
 const API_KEY = process.env.REACT_APP_FLIGHT_API_KEY
@@ -18,13 +18,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-      
-        <FontAwesomeIcon icon={faPlane} size="4x" />
-        <p>
-          Our awesome flight app!
-        </p>
-        <input type="text" id="callsign" name="callsign" onChange={(e) => setFlightNumber(e.target.value)} />
-        <button type="submit" onClick={() => { onSubmit(flightNumber)}}>Sök</button>
+      <FontAwesomeIcon icon={faPlane} size="4x"/>
+        <form className="input-button-wrapper">
+          <input placeholder="Enter your flight number" pattern="([A-Za-z]{2}[0-9]{3,4}|[A-Za-z][0-9]{5})" required type="text" id="callsign" name="callsign" title="The flightnumber must be in one of the following patterns: A12345, AB123 or AB1234" onChange={(e) => setFlightNumber(e.target.value)}/>
+          <button type="submit" onClick={() => { onSubmit(flightNumber)}}><FontAwesomeIcon icon={faArrowRight}/></button>
+        </form>
+        <div className="input-helper-text">Valid flight numbers: AB123, AB1234 or A12345</div>
         {flightData && (
           <div>
             <p>{flightData[0].number}</p>
@@ -34,7 +33,6 @@ function App() {
             <p>To: {flightData[0].arrival.airport.name}</p>
           </div>
         )}
-        
       </header>
     </div>
   );
